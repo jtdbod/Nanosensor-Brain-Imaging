fae25ea63884ea794258326584886a424e7f3822
Unknown <jackson.travis.do@gmail.com>
2018-01-22 11:44:24 -0800

    add GUI fig and control file

--- nanosensor_imaging_GUI.m
+++ nanosensor_imaging_GUI.m
@@ -22,7 +22,7 @@ function varargout = nanosensor_imaging_GUI(varargin)
 
 % Edit the above text to modify the response to help nanosensor_imaging_GUI
 
-% Last Modified by GUIDE v2.5 18-Jan-2018 17:41:34
+% Last Modified by GUIDE v2.5 19-Jan-2018 13:38:06
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -77,7 +77,57 @@ varargout{1} = handles.output;
 function loadbutton_Callback(hObject, eventdata, handles)
 % hObject    handle to loadbutton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
+
+%Load file
 [FileName,PathName,FilterINdex] = uigetfile();
+handles.dataset = load(strcat(PathName,'/',FileName));
+set(handles.CurrentFileLoaded, 'String', FileName);
+
+%Plot file
+
+subplot(131)
+    [B,L,N,A] = bwboundaries(handles.dataset.mask,'noholes');
+    imagesc(handles.dataset.imagemed); hold on;
+    colors=['b' 'g' 'r' 'c' 'm' 'y'];
+    for k=1:length(B),
+      boundary = B{k};
+      cidx = mod(k,length(colors))+1;
+      plot(boundary(:,2), boundary(:,1),...
+           colors(cidx),'LineWidth',1);
+
+      %randomize text position for better visibility
+      rndRow = ceil(length(boundary)/(mod(rand*k,7)+1));
+      col = boundary(rndRow,2); row = boundary(rndRow,1);
+      h = text(col+1, row-1, num2str(L(row,col)));
+      set(h,'Color',colors(cidx),'FontSize',14);
+    end
+    subplot(132)
+    for tracenum=1:size(handles.dataset.measuredValues,1)
+        %%%%NEED TO WORK ON THIS BASELINE CORRECTION AND NORMALIZATION
+        signal = handles.dataset.measuredValues(tracenum,:)+abs(min(handles.dataset.measuredValues(tracenum,:)));
+        signal = signal./max(signal);
+        traces(tracenum,:)=signal;
+    end
+        
+    x = 1:size(traces,2);
+    x=x./handles.frameRate;
+    for trace=1:size(traces,1)
+        %smoothed=smooth(traces(trace,:),'rloess');
+        plot(x,traces(trace,:)+trace-1);
+        %plot(x,smoothed+trace-1)
+        text(0,trace,num2str(trace));
+        hold on
+    end
+    xlabel('Time (s)')
+    ylabel('Normalized Intensity (a.u.)')
+    
+    subplot(133)
+    imagesc(traces)
+    ylabel('ROI#');
+    xlabel('Time (s)');
+
+
+
 
 
 % --- Executes on button press in processfilebutton.
@@ -144,6 +194,8 @@ function enterframerate_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of enterframerate as text
 %        str2double(get(hObject,'String')) returns contents of enterframerate as a double
 
+frameRate = get(hObject,'String');
+handles.frameRate = str2double(frameRate);
 
 % --- Executes during object creation, after setting all properties.
 function enterframerate_CreateFcn(hObject, eventdata, handles)
@@ -157,7 +209,8 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
-
+frameRate = get(hObject,'String');
+handles.frameRate = str2double(frameRate);
 
 function edit4_Callback(hObject, eventdata, handles)
 % hObject    handle to edit4 (see GCBO)
@@ -186,3 +239,10 @@ function savedatabutton_Callback(hObject, eventdata, handles)
 % hObject    handle to savedatabutton (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function CurrentFileLoaded_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to CurrentFileLoaded (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
