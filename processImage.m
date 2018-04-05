0e1cdb82c01ad9011a7e28873796853bf934a39e
Jackson Travis Del Bonis-O'Donnell <jackson.travis.do@gmail.com>
2018-04-05 11:02:20 -0700

    Tweak ROI segmentation process

--- processImage.m
+++ processImage.m
@@ -1,17 +1,25 @@
 function [Lmatrix,mask,imagemed]=processImage(imagestack,strelsize,numopens)
-    imagestd=std(imagestack,0,3);
-    imagemed=medfilt2(imagestd);
-
+    imagestd=std(imagestack,0,3); %Standard deviation of video stack for each pixel
+    imagemed=medfilt2(imagestd); %Smoothing filter to eliminate noise.
+    
+    %Testing: normalize the SD to the mean pixel intensity.
+    imageavg=mean(imagestack,3);
     
     image = imagemed./max(imagemed(:));
-    mask1 = imbinarize(image,(mean2(image)+3*std2(image)));%, 'adaptive','ForegroundPolarity','bright','Sensitivity',0.5);
+    image2 = imadjust(image);
+    image2 = medfilt2(image2);
+    mask1 = imbinarize(image2,(mean2(image2)+3*std2(image2)));%, 'adaptive','ForegroundPolarity','bright','Sensitivity',0.5);
+    mask1 = bwareaopen(mask1,10); %Remove ROIs with fewer than 10 pixels
     %The above threshold is too high for the Ninox data
+    %thresh=multithresh(image2);
     se = strel('disk',strelsize);
-    
+    mask2=mask1;
+    %{
     for opens=1:numopens
-        mask1=imopen(mask1,se);
+        mask2=imopen(mask2,se);
     end
-    mask=mask1;
+    %}
+    mask=mask2;
     CC = bwconncomp(mask);
     Lmatrix = labelmatrix(CC);
 end
