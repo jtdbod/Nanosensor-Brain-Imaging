ce49d3dc68a02730fa80c7d7d68c731eb698b77d
Unknown <jackson.travis.do@gmail.com>
2019-01-08 15:35:54 -0800

    Move projection image calculations to when stack is loaded

--- nanosensor_imaging_GUI.m
+++ nanosensor_imaging_GUI.m
@@ -158,14 +158,39 @@ elseif strmatch(fileType,'tif') && any(FileName)
     handles.DataSet.projectionImages.f0 = mean(imageStack(:,:,1:5),3);
     fileinfo=imfinfo(strcat(PathName,'/',FileName));
     handles.DataSet.numFrames=size(fileinfo,1);
-    %The following is now part of the "Generate ROI" button. Mean and dFmax
-    %projections will be calculated there.
-    %{
-    %Generate maximum dF projection to use for further processing
-    dfStackMaxSmoothNorm = processImage(imageStack,handles);
-    %Assign max dF projection into the guidata handles
-    handles.DataSet.projectionImages.dFMaxProj = dfStackMaxSmoothNorm;
-    %}
+    %Generate Mean Projection and dF Max Projecitons and store in handles
+    if ~isfield(handles.DataSet.projectionImages,'meanProj')
+    currFig = gcf;
+    axes(handles.axes1);
+    cla(handles.axes1);
+    title('Calculating Mean Projection')
+    xlabel('')
+    imageStack = handles.ImageStack;
+    imstack = imageStack;
+    if any(imageStack(:)<0)
+        imstack = imageStack-min(imageStack(:));
+    end
+    
+    meanProjImage = mean(imstack,3);
+    meanProjImageFilt = medfilt2(meanProjImage,[3 3]);
+    handles.DataSet.projectionImages.meanProj = meanProjImageFilt;
+    guidata(hObject,handles);
+    end
+
+    if ~isfield(handles.DataSet.projectionImages,'dFMaxProj')
+    currFig = gcf;
+    axes(handles.axes1);
+    cla(handles.axes1);
+    title('Calculating dF Projection')
+    xlabel('')
+    imstack = handles.ImageStack;
+    dFImage = imstack-handles.DataSet.projectionImages.meanProj;
+    maxdFProjImage = max(dFImage,[],3);
+    maxdFProjImageFilt = medfilt2(maxdFProjImage,[4 4]);
+    handles.DataSet.projectionImages.dFMaxProj = maxdFProjImageFilt;
+    guidata(hObject,handles);
+    end
+    clear imstack dFImage
     guidata(hObject,handles);%To save DataSet to handles
 
     %Display first frame after file loads.
@@ -883,41 +908,6 @@ function CalculateROIs_Callback(hObject, eventdata, handles)
 
 strelsize=get(handles.strelSlider,'Value');
 numopens=get(handles.numopens_slider,'Value');
-
-
-if ~isfield(handles.DataSet.projectionImages,'meanProj')
-    currFig = gcf;
-    axes(handles.axes2);
-    cla(handles.axes2);
-    title('Calculating Mean Projection')
-    xlabel('')
-    imageStack = handles.ImageStack;
-    imstack = imageStack;
-    if any(imageStack(:)<0)
-        imstack = imageStack-min(imageStack(:));
-    end
-    
-    meanProjImage = mean(imstack,3);
-    meanProjImageFilt = medfilt2(meanProjImage,[3 3]);
-    handles.DataSet.projectionImages.meanProj = meanProjImageFilt;
-    guidata(hObject,handles);
-end
-
-if ~isfield(handles.DataSet.projectionImages,'dFMaxProj')
-    currFig = gcf;
-    axes(handles.axes2);
-    cla(handles.axes2);
-    title('Calculating dF Projection')
-    xlabel('')
-    imstack = handles.ImageStack;
-    dFImage = imstack-handles.DataSet.projectionImages.meanProj;
-    maxdFProjImage = max(dFImage,[],3);
-    maxdFProjImageFilt = medfilt2(maxdFProjImage,[4 4]);
-    handles.DataSet.projectionImages.dFMaxProj = maxdFProjImageFilt;
-    guidata(hObject,handles);
-end
-
-clear imstack dFImage
 
 [roiMask,~]=calculateMask(handles);
 if max(roiMask)==0
