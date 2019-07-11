95f4cfb1f00ae3c76c7eeed9995e66e41f963bf7
Unknown <jackson.travis.do@gmail.com>
2019-07-11 16:44:09 -0700

    Fix 'color ROIs by tau/peak intensity'

--- plotResults.m
+++ plotResults.m
@@ -38,13 +38,13 @@ else
         cidx = mod(cidx,length(colors))+1; %Cycle through colors for drawing borders
         for k=1:length(B),
           boundary = B{k};
-          %cidx = mod(k,length(colors))+1;
+          cidx = mod(k,length(colors))+1;
           plot(boundary(:,2), boundary(:,1),...
                colors(cidx),'LineWidth',1);
           %randomize text position for better visibility
           rndRow = ceil(length(boundary)/(mod(rand*k,7)+1));
           col = boundary(rndRow,2); row = boundary(rndRow,1);
-          %h = text(col+1, row-1, num2str(L(row,col)));
+          h = text(col+1, row-1, num2str(L(row,col)));
           h = text(col+1, row-1, num2str(roi));
           set(h,'Color',colors(cidx),'FontSize',14);
         end
