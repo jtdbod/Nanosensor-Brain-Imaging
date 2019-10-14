7cdee8af098a30cc19254d8d6809a75511390080
Unknown <jackson.travis.do@gmail.com>
2019-10-14 14:04:14 -0700

    Fix commit issues

--- processTrace.m
+++ processTrace.m
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
+
 function [zscoreTrace] = processTrace(trace,frameRate,stimFrameNumber)
-=======
-function [zscoreTrace] = processTrace(trace,frameRate,stimFrame)
->>>>>>> dev
+
 %trace = currentDataset.measuredValues(1).MeanIntensity;
 x=1:length(trace);
 window = 20; %seconds
 frameWindow = window*frameRate;
 
-<<<<<<< HEAD
 xbase = [x(1:stimFrameNumber-10),x(stimFrameNumber+50:end)];
 baseline = interp1(xbase,trace(xbase),1:length(x),'loess');
-=======
-xbase = [x(1:stimFrame-10),x(end-50:end)];
-baseline = interp1(xbase,trace(xbase),1:length(trace),'loess');
->>>>>>> dev
+
 smoothTrace = smooth(baseline,frameWindow);
 
 centeredTrace = trace-smoothTrace';
